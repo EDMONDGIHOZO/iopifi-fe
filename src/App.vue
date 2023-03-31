a68bebc0319099d51fa6f54b7eb8dc7a301c5b27
EDMONDGIHOZO <edmond@gojabu.com>
2023-03-31 12:17:16 +0200

    initialize with layout components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
 import {RouterView} from 'vue-router'
+import TopBar from "@/components/layout/TopBar.vue";
+import LeftMenu from "@/components/layout/LeftMenu.vue";
 </script>
 
 <template>
-  <RouterView/>
+  <main>
+    <top-bar/>
+    <section>
+      <left-menu/>
+      <div class="content_wrapper">
+        <RouterView/>
+      </div>
+    </section>
+  </main>
 </template>
 
---
+++ src/components/layout/LeftMenu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    left menu
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'left-menu',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
