461484eeeade724ed612d2fac6cdda667a8e89b8
EDMONDGIHOZO <edmond@gojabu.com>
2023-03-31 12:14:18 +0200

    start project - install packages

---
+++ src/views/PatientsView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>patients view</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'patients-view',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
